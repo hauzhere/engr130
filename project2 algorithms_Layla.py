a28c507941ba5fcd80be4b14a5cfcce4f8d2cd1a
hauzhere <161328793+hauzhere@users.noreply.github.com>
2025-03-10 04:43:21 -0400

    Add files via upload
    UPDATED COMMENT

--- project2 algorithms_Layla.py
+++ project2 algorithms_Layla.py
@@ -37,68 +37,67 @@ def move_backward():
     turn_left()
     turn_left()
 
-def findGoal():
+def findGoal(): #the user-function to find the coordinates of the goal
     for row in range(row_size):
-        for col in range(col_size):
+        for col in range(col_size): #loop through each cell and row in the array
             if arr_map[row][col] == 99:
-                return row,col
+                return row,col #return the coordinates of the goal
 
 
-def findRobot():
+def findRobot(): #the user-function to find the coordinates of the robot
     for row in range(row_size):
-        for col in range(col_size):
+        for col in range(col_size): #loop through each cell and row in the array
             if arr_map[row][col] == 2:
-                arr_map[row][col] = -99 #change symbol for robot position
-                return row,col
+                arr_map[row][col] = -99 #change symbol for robot position so as not to be confused with other symbols
+                return row,col #return the coordinates of the robot
 
 
     
-def waveShoot():
-    row_goal, col_goal = findGoal()
-    next = [(row_goal, col_goal)]
-    x = 1
+def waveShoot(): #the user-function to shoot waves marking incrementing steps spreading outward from one cell to its adjacent cells, marking and replacing the value of each cell with the wavestep number indicating how many steps it takes for the robot to reach said cell if it starts from the goal
+    row_goal, col_goal = findGoal() #run the function to find the coordinates of the goal
+    next = [(row_goal, col_goal)] #An array of tuples storing the coordinates (rows and columns) of the cells in the current wavestep. Initialized with the cooridnates of the goal because the first wavestep is shot outward from the goal to its adjacent cells
+    x = 1 #marks the wavestep (how many steps taken to reach a cell from the goal)
     directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # Up, Down, Left, Right
 
-    while next:
-        new_position = []
-        count = 0 
-        for row, col in next:
-            for row_direct, col_direct in directions:
+    while next: #continue until all the cells in the current step has been accounted for
+        new_position = [] #initialize an array to store the cells in the new wavestep
+        count = 0 #variable to store the number of cells in the current wavestep that already shot waves to adjacent cells
+        for row, col in next: #loop through each cell in the current wavestep
+            for row_direct, col_direct in directions: #loop through each of the 4 directions possible in which the wave can shoot
                 new_row = row + row_direct
-                new_col = col + col_direct
-                if (0 <= new_row < row_size) and (0 <= new_col < col_size) and arr_map[new_row][new_col] != -1 and (arr_map[new_row][new_col] == 0):
-                    new_position.append((new_row, new_col))
-                    arr_map[new_row][new_col] = x
-            count += 1  
-        if count == len(next):
-            next = new_position  
-            x += 1  
-        else:
-            break
+                new_col = col + col_direct  #coordinates of each adjacent cells the wave can shoot to
+                if (0 <= new_row < row_size) and (0 <= new_col < col_size) and arr_map[new_row][new_col] != -1 and (arr_map[new_row][new_col] == 0): #ensure the adjacent cell is still within the map, is not a barrier, and is an open space
+                    new_position.append((new_row, new_col)) #if the condition above is satisfied, add the adjacent cell to the next wavestep
+                    arr_map[new_row][new_col] = x #marks the cell with the corresponding wavestep
+            count += 1  #increment for each cell in the current wavestep that already shot waves to adjacent cells
+        if count == len(next): #only move on to the cells in the next wavestep if all the cells in the current wavestep have shot wave to adjacent cells
+            next = new_position  #replace cells in the current wavestep with its valid adjacent cells for the next wavestep
+            x += 1  #an extra step is taken
+        
 waveShoot()
 for row in arr_map:
-    print(row)           
-       
-def wavefrontSearch():
-    row_robot, col_robot = findRobot()
-    waveShoot()
+    print(row)   
+              
+def wavefrontSearch(): #the robot traces the lowest-numbered cells back to the goal to ensure it reaches the goal in the most efficient path (least steps taken) 
+    row_robot, col_robot = findRobot() #run the function to find the coordinates of the robot
+    waveShoot() #run the function to shoot waves
     directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # Up, Down, Left, Right
    
-    while arr_map[row_robot][col_robot] != 99: 
-        move = ()
-        min_val = 300
+    while arr_map[row_robot][col_robot] != 99: #continue while the goal has not been reached
+        move = () #the tuple containing coordinates that indicate the direction in which the robot should move
+        min_val = 300 #a hypothetical value to initialize the current lowest-numbered adjacent cell
 
-        for row_direct, col_direct in directions:
+        for row_direct, col_direct in directions: #loop through each of the 4 directions possible in which the wave can shoot
             new_row = row_robot + row_direct
-            new_col = col_robot + col_direct
-            if 0 <= new_row < row_size and 0 <= new_col < col_size and arr_map[new_row][new_col] != -1:
-                if arr_map[new_row][new_col] < min_val and arr_map[new_row][new_col] > 0:
-                    min_val = arr_map[new_row][new_col]
-                    move = (row_direct, col_direct)
+            new_col = col_robot + col_direct #coordinates of each adjacent cells the wave can shoot to
+            if 0 <= new_row < row_size and 0 <= new_col < col_size and arr_map[new_row][new_col] != -1: #ensure the adjacent cell is still within the map and is not a barrier
+                if arr_map[new_row][new_col] < min_val and arr_map[new_row][new_col] > 0: #find the next lowest-numbered cell
+                    min_val = arr_map[new_row][new_col] 
+                    move = (row_direct, col_direct) #if is the new lowest-numbered cell, replace the current lowest-numbered cell and store the direction to reach that adjacent cell
         
         if move:
             row_robot += move[0]
-            col_robot += move[1]
+            col_robot += move[1] 
             if move == directions[0]:
                 move_backward()
             elif move == directions[1]:
@@ -111,7 +110,7 @@ def wavefrontSearch():
                 turn_right()
                 move_forward()
                 turn_left()
-
+            #the robot goes to the lowest-adjacent cell. update the coordinates of the robot
 
 
         
